15518a519c05a621251820af0e10f46fa99a729f
alias <nitishkumarchauhan1996@gmail.com>
2020-11-30 23:27:07 +0530

    Day 5:Trees

--- Day 5/Day_5.md
+++ Day 5/Day_5.md
@@ -2,28 +2,45 @@
 
 ## Day 5
 
-#### mostly it has functions for AVL search trees
+#### Mostly it has functions for AVL trees
 
 ### The functions applied in day 5 :
 ```sh
     int heightAVL(struct avlnode *head);
-
+    
     struct avlnode *getNewNode(int data,int height);
-
+    
     struct avlnode *singleLRotation(struct avlnode *head);
-
+    
     struct avlnode *singleRRotation(struct avlnode *head);
-
+    
     struct avlnode *doubleLRRotation(struct avlnode *head);
-
+    
     struct avlnode *doubleRLRotation(struct avlnode *head);
+    
+    struct avlnode *insertNode(struct avlnode *head,int data);
+    
+    struct avlnode *generateAVL(int h);
+    
+    struct avlnode *HB0(int h);
+    
+    int isAVL(struct avlnode* head);
+    
+    int closeInBST(struct avlnode *head,int data);
 
-    struct avlnode *insertNode(struct avlnode *head,struct avlnode *parent,int data);
+```
 
+### The funtion applied from day 0:
+```sh
+    void printNodesInorder(struct avlnode *temp);
 ```
 
 ## Output:
 ```sh
-
-
+    The data of head after generating the AVL tree giving only height: 5
+    Inorder print: 1, 2, 3, 4, 5, 6, 7, 
+    The tree generated above is avl or not: True
+    The closest value in the Acting-BST Avl tree to 10 is: 7
+    Inorder print of the AVL tree created using array is given: 1, 2, 3, 4, 5, 8, 
+    The tree generated above is avl or not: True
 ```
--- Day 5/day5.cpp
+++ Day 5/day5.cpp
@@ -14,6 +14,22 @@ struct avlnode{
     struct avlnode *right;
 };
 
+// Global variable for generation of AVL of height h with minimum node 
+int g_data=1;
+
+// previously made function reused decleration
+
+void printNodesInorder(struct avlnode *head);
+
+// Function previous definition
+void printNodesInorder(struct avlnode *temp){
+    if(!temp) return;
+    printNodesInorder(temp->left);
+    cout<<temp->data<<", ";
+    printNodesInorder(temp->right);
+    return;
+}
+
 // Functions Decleration
 int heightAVL(struct avlnode *head);
 struct avlnode *getNewNode(int data,int height);
@@ -21,7 +37,11 @@ struct avlnode *singleLRotation(struct avlnode *head);
 struct avlnode *singleRRotation(struct avlnode *head);
 struct avlnode *doubleLRRotation(struct avlnode *head);
 struct avlnode *doubleRLRotation(struct avlnode *head);
-struct avlnode *insertNode(struct avlnode *head,struct avlnode *parent,int data);
+struct avlnode *insertNode(struct avlnode *head,int data);
+struct avlnode *generateAVL(int h);
+struct avlnode *HB0(int h);
+int isAVL(struct avlnode* head);
+int closeInBST(struct avlnode *head,int data);
 
 // Function Definition
 int heightAVL(struct avlnode *head){
@@ -73,29 +93,89 @@ struct avlnode *doubleRLRotation(struct avlnode *head){
     return singleRRotation(temp);
 }
 
-struct avlnode *insertNode(struct avlnode *head,struct avlnode *parent,int data){
-    if(!head) root=getNewNode(data,0);
+struct avlnode *insertNode(struct avlnode *head,int data){
+    if(!head) return getNewNode(data,0);
     if(head->data>=data){
-        head->left=insertNode(head->left,root,data);
+        head->left=insertNode(head->left,data);
         if((heightAVL(head->left)-heightAVL(head->right))>1){
             if(head->left->data>data) head=singleLRotation(head);
             else head=doubleLRRotation(head);   
         }
     }else{
-        head->right=insertNode(head->right,root,data);
+        head->right=insertNode(head->right,data);
         if((heightAVL(head->left)-heightAVL(head->right))>1){
             if(head->right->data<data) head=singleRRotation(head);
             else head=doubleRLRotation(head);
         }
     }
     // height manipulation
-    head->height=max(heightAVL(newN->left),heightAVL(newN->right))+1;
+    head->height=max(heightAVL(head->left),heightAVL(head->right))+1;
     return head;
 }
 
+struct avlnode *generateAVL(int h){
+    struct avlnode *temp;
+    if(h<=0) return NULL;
+    temp=getNewNode(0,0);
+    temp->left=generateAVL(h-1);
+    temp->data=g_data++;
+    temp->right=generateAVL(h-2);
+    temp->height=heightAVL(temp);
+    return temp;
+}
 
+// HB0 is the binary tree where number of nodes at height h is (2^(h+1))-1
+// in this we will not use the height data-point of avlnode treating it as BST
+struct avlnode *HB0(int h){
+    struct avlnode *temp;
+    if(h<=0) return NULL;
+    temp=getNewNode(0,0);
+    temp->left=HB0(h-1);
+    temp->data=g_data++;
+    temp->right=HB0(h-2);
+    return temp;
+}
+
+// check if BST is AVL so height not used in this
+int isAVL(struct avlnode* head){
+    if(!head) return 0;
+    int l=isAVL(head->left);
+    int r=isAVL(head->right);
+    if(l==-1||r==-1) return -1;
+    if(abs(l-r)<=1) return max(l+1,r+1);
+    else return -1;
+}
+
+// closest value in the BST in the given value...
+int closeInBST(struct avlnode *head,int data){
+    if(!head) return 99999;
+    int res;
+    int l=closeInBST(head->left,data);
+    int r=closeInBST(head->right,data);
+    if(abs(l-data)<=abs(r-data)) res=l;
+    else res=r;
+    if(abs(head->data-data)<abs(res-data)) res=head->data;
+    return res;
+}
 
 int main(int argc, char const *argv[]) {
-
+    int a[]={5,4,3,2,1,8};
+    struct avlnode *head=generateAVL(4);
+    cout<<"The data of head after generating the AVL tree giving only height: "<<head->data<<endl;
+    cout<<"Inorder print: ";
+    printNodesInorder(head);
+    cout<<endl;
+    string s=isAVL(head)>0?"True":"False";
+    cout<<"The tree generated above is avl or not: "<<s<<endl;
+    cout<<"The closest value in the Acting-BST Avl tree to 10 is: "<<closeInBST(head,10)<<endl;
+    head=NULL;
+    for(int x:a){
+        head=insertNode(head,x);
+    }
+    cout<<"Inorder print of the AVL tree created using array is given: ";
+    printNodesInorder(head);
+    cout<<endl;
+    s=isAVL(head)>0?"True":"False";
+    cout<<"The tree generated above is avl or not: "<<s<<endl;
     return 0;
 }
